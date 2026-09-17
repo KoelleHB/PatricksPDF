e242fb7285a483b0f5bcf54f928350f3b9edb16b
KoelleHB <pwjulius@gmail.com>
2026-09-17 08:13:43 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -40,7 +40,7 @@ export default defineConfig(() => {
           },
           file_handlers: [
             {
-              action: '/',
+              action: '/openFile',
               accept: {
                 'application/pdf': ['.pdf'],
               },
